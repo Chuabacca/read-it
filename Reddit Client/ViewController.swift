ec6204f432a985ad0480c42b0560dbf54895075b
Chuabacca <jonathan.chua@dollarshaveclub.com>
2017-10-12 14:51:37 -0700

    Initial commit and work.

--- Reddit Client/ViewController.swift
+++ Reddit Client/ViewController.swift
@@ -6,10 +6,20 @@
 //  Copyright © 2017 Jonathan Chua. All rights reserved.
 //
 
+// x guard against empty string
+// x if empty display error "please enter subreddit"
+// x and make button red
+// x if valid entry, remove error
+
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var subredditField: UITextField!
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if subredditField.text == "" {
+            goButton.backgroundColor = UIColor.red
+            errorMessageLabel.text = "Please enter a valid subreddit."
+            return false
+        }
+        else {
+            goButton.backgroundColor = UIColor( red: 0, green: 0.478, blue:1, alpha: 1.0 )
+            errorMessageLabel.text = ""
+            return true
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let postsViewController = segue.destination as? PostsTableViewController {
+        postsViewController.subreddit = subredditField.text
+        }
+    }
+    
 }
 
